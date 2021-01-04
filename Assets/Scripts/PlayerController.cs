4e57c083afee09a0bee5c470a57b2a7b7c8f735b
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-04 12:05:36 -0800

    t1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody Rb3D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb3D = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 roll = new Vector3(horizontal, 0, vertical);
+        roll = roll * speed * Time.deltaTime;
+        Rb3D.AddForce(roll);
+    }
+}
